4cd697836ead5b98f9064e1ca7336cf23988f4e8
Jordi <jordi.id96@gmail.com>
2024-06-19 18:08:30 +0200

    probe z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1592,7 +1592,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -35, -9.5, -2.750 }
+#define NOZZLE_TO_PROBE_OFFSET { -35, -9.5, -2.850 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
